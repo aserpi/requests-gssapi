44fcb305c9c3174441102aa62e56643ff46fe751
Alessandro Serpi <alessandro.serpi@me.com>
2021-07-02 12:48:41 +0200

    Add proxy tests
    Signed-off-by: Alessandro Serpi <alessandro.serpi@me.com>

--- test_requests_gssapi.py
+++ test_requests_gssapi.py
@@ -216,6 +216,43 @@ class GSSAPITestCase(unittest.TestCase):
                 creds=None, mech=None, flags=gssflags, usage="initiate")
             fake_resp.assert_called_with(b"token")
 
+    def test_handle_407(self):
+        with patch.multiple("gssapi.SecurityContext", __init__=fake_init,
+                            step=fake_resp):
+            response_ok = requests.Response()
+            response_ok.url = "http://www.example.org/"
+            response_ok.status_code = 200
+            response_ok.headers = {'proxy-authenticate': b64_negotiate_server}
+
+            connection = Mock()
+            connection.send = Mock(return_value=response_ok)
+
+            raw = Mock()
+            raw.release_conn = Mock(return_value=None)
+
+            request = requests.Request()
+            response = requests.Response()
+            response.request = request
+            response.url = "http://www.example.org/"
+            response.headers = {'proxy-authenticate': b64_negotiate_token}
+            response.status_code = 407
+            response.connection = connection
+            response._content = ""
+            response.raw = raw
+            auth = requests_gssapi.HTTPKerberosAuth()
+            r = auth.handle_407(response)
+
+            self.assertTrue(response in r.history)
+            self.assertEqual(r, response_ok)
+            self.assertEqual(request.headers['Proxy-Authorization'],
+                             b64_negotiate_response)
+            connection.send.assert_called_with(request)
+            raw.release_conn.assert_called_with()
+            fake_init.assert_called_with(
+                name=gssapi_sname("HTTP@www.example.org"),
+                creds=None, mech=None, flags=gssflags, usage="initiate")
+            fake_resp.assert_called_with(b"token")
+
     def test_authenticate_server(self):
         with patch.multiple("gssapi.SecurityContext", __init__=fake_init,
                             step=fake_resp):
@@ -455,6 +492,48 @@ class GSSAPITestCase(unittest.TestCase):
                 usage="initiate", flags=gssflags, creds=None, mech=None)
             fake_resp.assert_called_with(b"token")
 
+    def test_handle_response_407(self):
+        # Get a 407 from server, authenticate, and get a 200 back.
+        with patch.multiple("gssapi.SecurityContext", __init__=fake_init,
+                            step=fake_resp):
+            response_ok = requests.Response()
+            response_ok.url = "http://www.example.org/"
+            response_ok.status_code = 200
+            response_ok.headers = {'proxy-authenticate': b64_negotiate_server}
+
+            connection = Mock()
+            connection.send = Mock(return_value=response_ok)
+
+            raw = Mock()
+            raw.release_conn = Mock(return_value=None)
+
+            request = requests.Request()
+            response = requests.Response()
+            response.request = request
+            response.url = "http://www.example.org/"
+            response.headers = {'proxy-authenticate': b64_negotiate_token}
+            response.status_code = 407
+            response.connection = connection
+            response._content = ""
+            response.raw = raw
+
+            auth = requests_gssapi.HTTPKerberosAuth()
+            auth.handle_other = Mock(return_value=response_ok)
+
+            r = auth.handle_response(response)
+
+            self.assertTrue(response in r.history)
+            auth.handle_other.assert_called_once_with(response_ok)
+            self.assertEqual(r, response_ok)
+            self.assertEqual(request.headers['Proxy-Authorization'],
+                             b64_negotiate_response)
+            connection.send.assert_called_with(request)
+            raw.release_conn.assert_called_with()
+            fake_init.assert_called_with(
+                name=gssapi_sname("HTTP@www.example.org"),
+                usage="initiate", flags=gssflags, creds=None, mech=None)
+            fake_resp.assert_called_with(b"token")
+
     def test_handle_response_401_rejected(self):
         # Get a 401 from server, authenticate, and get another 401 back.
         # Ensure there is no infinite recursion.
@@ -490,6 +569,49 @@ class GSSAPITestCase(unittest.TestCase):
 
             self.assertEqual(r.status_code, 401)
             self.assertEqual(request.headers['Authorization'],
+                             b64_negotiate_response)
+            connection.send.assert_called_with(request)
+            raw.release_conn.assert_called_with()
+            fake_init.assert_called_with(
+                name=gssapi_sname("HTTP@www.example.org"),
+                usage="initiate", flags=gssflags, creds=None, mech=None)
+            fake_resp.assert_called_with(b"token")
+
+    def test_handle_response_407_rejected(self):
+        # Get a 407 from server, authenticate, and get another 407 back.
+        # Ensure there is no infinite recursion.
+        with patch.multiple("gssapi.SecurityContext", __init__=fake_init,
+                            step=fake_resp):
+            connection = Mock()
+
+            def connection_send(self, *args, **kwargs):
+                reject = requests.Response()
+                reject.url = "http://www.example.org/"
+                reject.status_code = 407
+                reject.connection = connection
+                return reject
+
+            connection.send.side_effect = connection_send
+
+            raw = Mock()
+            raw.release_conn.return_value = None
+
+            request = requests.Request()
+            response = requests.Response()
+            response.request = request
+            response.url = "http://www.example.org/"
+            response.headers = {'proxy-authenticate': b64_negotiate_token}
+            response.status_code = 407
+            response.connection = connection
+            response._content = ""
+            response.raw = raw
+
+            auth = requests_gssapi.HTTPKerberosAuth()
+
+            r = auth.handle_response(response)
+
+            self.assertEqual(r.status_code, 407)
+            self.assertEqual(request.headers['Proxy-Authorization'],
                              b64_negotiate_response)
             connection.send.assert_called_with(request)
             raw.release_conn.assert_called_with()
